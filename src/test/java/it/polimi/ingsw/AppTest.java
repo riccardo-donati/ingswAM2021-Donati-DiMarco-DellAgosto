1ed0082223f6b27c9084847502c018cafce270b1
jack <giacomo.dellagosto@gmail.com>
2021-03-06 13:55:42 +0100

    Changed junit dependency to junit-jupiter in pom.xml and fixed import directories in example test class AppTest

--- src/test/java/it/polimi/ingsw/AppTest.java
+++ src/test/java/it/polimi/ingsw/AppTest.java
@@ -1,12 +1,8 @@
 package it.polimi.ingsw;
 
-import static org.junit.Assert.assertTrue;
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
 
-import org.junit.Test;
-
-/**
- * Unit test for simple App.
- */
 public class AppTest 
 {
     /**
