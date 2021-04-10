5e594cc27a6fe784f36c149a327f3176f724555d
Davidedm99 <51082176+Davidedm99@users.noreply.github.com>
2021-04-10 15:37:33 +0200

    Made some tests on Singleplayer and Multiplayer to check the response of the FaithPath and the PopeFavors.

--- src/test/java/it/polimi/ingsw/model/SingleplayerTest.java
+++ src/test/java/it/polimi/ingsw/model/SingleplayerTest.java
@@ -20,9 +20,9 @@ class SingleplayerTest {
     }
 
     @Test
-    public void TestPath(){
+    public void TestPath(){                                 //
         s = new Singleplayer();
-        s.addPlayer("Riccardo Fenati");
+        s.addPlayer("Riccardo Ricevuti");
         s.getPlayers().get(0).getBoard().getFaithPath().addToPosition(5);
         s.pushBlackCross(9);
         assertEquals(s.getPlayers().get(0).getBoard().getFaithPath().getPopeFavorList().get(0).getState(), PopeFavorState.ACTIVE);
@@ -31,5 +31,36 @@ class SingleplayerTest {
         s.pushBlackCross(7);
         assertEquals(s.getPlayers().get(0).getBoard().getFaithPath().getPopeFavorList().get(1).getState(), PopeFavorState.DISCARDED);
         assertEquals(s.getPlayers().get(0).getBoard().getFaithPath().getPopeFavorList().get(2).getState(), PopeFavorState.UNACTIVE);
+    }
+
+    @Test
+    public void TestPath2(){                                                        //la bCross raggiunge prima la fine, ma il player è nella zona quindi deve essere attivo
+        s = new Singleplayer();                                                     //il player raggiung eil secondo stato e si attiva, ma la croce raggiunge lo stato finale
+        s.addPlayer("Giacomo del Luglio");
+        s.getPlayers().get(0).getBoard().getFaithPath().addToPosition(6);
+        s.pushBlackCross(8);
+        s.getPlayers().get(0).getBoard().getFaithPath().addToPosition(11);
+        assertEquals(s.getPlayers().get(0).getBoard().getFaithPath().getPopeFavorList().get(0).getState(), PopeFavorState.ACTIVE);
+        assertEquals(s.getPlayers().get(0).getBoard().getFaithPath().getPopeFavorList().get(1).getState(), PopeFavorState.ACTIVE);
+
+        s.pushBlackCross(17);
+        assertEquals(s.getPlayers().get(0).getBoard().getFaithPath().getPopeFavorList().get(2).getState(), PopeFavorState.DISCARDED);
+    }
+
+    @Test
+    public void TestPath3(){
+        s = new Singleplayer();
+        s.addPlayer("Davide di Davide");            //mi assicuro che anche se il player passi sulle caselle lo stato non cambi
+        s.pushBlackCross(23);
+        assertEquals(s.getPlayers().get(0).getBoard().getFaithPath().getPopeFavorList().get(0).getState(), PopeFavorState.DISCARDED);
+        assertEquals(s.getPlayers().get(0).getBoard().getFaithPath().getPopeFavorList().get(1).getState(), PopeFavorState.DISCARDED);
+        s.getPlayers().get(0).getBoard().getFaithPath().addToPosition(8);
+        assertFalse(s.getPlayers().get(0).getBoard().getFaithPath().getPopeFavorList().get(0).getState().equals(PopeFavorState.ACTIVE));
+        assertFalse(s.getPlayers().get(0).getBoard().getFaithPath().getPopeFavorList().get(0).getState().equals(PopeFavorState.UNACTIVE));
+        s.getPlayers().get(0).getBoard().getFaithPath().addToPosition(15);
+        assertFalse(s.getPlayers().get(0).getBoard().getFaithPath().getPopeFavorList().get(1).getState().equals(PopeFavorState.ACTIVE));
+        assertFalse(s.getPlayers().get(0).getBoard().getFaithPath().getPopeFavorList().get(1).getState().equals(PopeFavorState.UNACTIVE));
+        s.pushBlackCross(3);                                //la bcross vince ma il player è nella zona
+        assertEquals(s.getPlayers().get(0).getBoard().getFaithPath().getPopeFavorList().get(2).getState(), PopeFavorState.ACTIVE);
     }
 }
